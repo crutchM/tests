eb8e6f5d8ae1229d31a118f2ce4c9174d56a5f43
crutchm <sads1100@gmail.com>
2022-07-01 13:58:28 +0500

    added publisher script with source in json file

--- l0/publisher/main.go
+++ l0/publisher/main.go
@@ -1,1 +1,23 @@
-package publisher
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"time"
+
+	"github.com/nats-io/stan.go"
+)
+
+func main() {
+	sc, err := stan.Connect("test-cluster", "stan-pub")
+	if err != nil {
+		log.Fatal("can not connect to stan")
+	}
+	defer sc.Close()
+	file, _ := ioutil.ReadFile("model.json")
+
+	_ = sc.Publish("l0-task", file)
+	fmt.Println("успешно отправлено")
+	time.Sleep(5 * time.Second)
+}
